673a048149b136d24d5400dab3c35400c60403ee
ktarrant <kmtarrant13@gmail.com>
2025-06-25 00:07:39 -0400

    cleanup Main.gd

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -10,10 +10,6 @@ const Player = preload("res://scripts/Player.gd")
 
 func _ready():
 	$Camera2D.players = [$Player1, $Player2, $Player3, $Player4]
-	#$UI/PlayerUI1.player = $Player1
-	#$UI/PlayerUI2.player = $Player2
-	#$UI/PlayerUI3.player = $Player3
-	#$UI/PlayerUI4.player = $Player4
 
 func _on_DeathZone_body_entered(body):
 	if body is Player:
