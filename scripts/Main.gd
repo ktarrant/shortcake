f73273e26a0cc9e8b78287ea711010e5e832ea87
ktarrant <kmtarrant13@gmail.com>
2025-06-25 01:06:52 -0400

    escape to exit

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -9,8 +9,14 @@ const Player = preload("res://scripts/Player.gd")
 
 
 func _ready():
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	
 	$Camera2D.players = [$Player1, $Player2, $Player3, $Player4]
 
 func _on_DeathZone_body_entered(body):
 	if body is Player:
 		body.respawn(spawn_point.global_position)
+
+func _unhandled_input(event):
+	if event is InputEventKey and event.pressed and event.keycode == KEY_ESCAPE:
+		get_tree().quit()
