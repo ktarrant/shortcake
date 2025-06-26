b77d48324d05caa2bafb3669b4b98a2b2c38e6aa
ktarrant <kmtarrant13@gmail.com>
2025-06-25 22:07:24 -0400

    basic PlayerSelect screen

--- scripts/PlayerSelect.gd
+++ scripts/PlayerSelect.gd
@@ -1,21 +1,42 @@
 extends Control
 
-var ready_players := {}
-var joystick_ids := []
+var player_joypads := {1: null, 2: null, 3: null, 4: null}
+var dummy_count := 0
 
 func _ready():
-	joystick_ids = Input.get_connected_joypads()
-	for id in joystick_ids:
-		ready_players[id] = false
+	update_display()
+	$VBoxContainer/StartButton.pressed.connect(_on_start_pressed)
+	$VBoxContainer/AddDummyButton.pressed.connect(_on_add_dummy_pressed)
 
-func _process(delta):
-	for id in joystick_ids:
-		if Input.is_joy_button_pressed(id, JOY_BUTTON_START) and not ready_players[id]:
-			ready_players[id] = true
-			print("Player %d ready" % id)
+func _input(event):
+	if event is InputEventJoypadButton and event.pressed:
+		var new_id = event.device
+		for i in range(2, 5):  # player 2 to 4
+			if player_joypads[i] == null and not new_id in player_joypads.values():
+				player_joypads[i] = new_id
+				update_display()
+				break
 
-	if ready_players.size() > 0 and ready_players.values().all(func(v): return v):
-		start_game()
+func update_display():
+	for i in range(1, 5):
+		var label = get_node("VBoxContainer/PlayerRow%d" % i)
+		if i == 1:
+			label.text = "Player 1: Keyboard & Mouse"
+		elif typeof(player_joypads[i]) == TYPE_INT:
+			label.text = "Player %d: JoyCon %d" % [i, player_joypads[i]]
+		elif player_joypads[i] == "Dummy":
+			label.text = "Player %d: Dummy" % i
+		else:
+			label.text = "Player %d: Not Connected" % i
 
-func start_game():
-	get_tree().change_scene_to_file("res://scenes/main/Main.tscn")
+func _on_add_dummy_pressed():
+	for i in range(2, 5):
+		if player_joypads[i] == null:
+			player_joypads[i] = "Dummy"
+			update_display()
+			break
+
+func _on_start_pressed():
+	var connected_players = player_joypads.values().filter(func(p): return p != null)
+	if connected_players.size() >= 1:
+		get_tree().change_scene_to_file("res://path_to_your_game_scene.tscn")
