52b31da4b7153c9f1d97a95be0f696f95d3dbe03
ktarrant <kmtarrant13@gmail.com>
2025-06-29 01:11:53 -0400

    add theme

--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -4,7 +4,7 @@ func _ready():
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	$HBoxContainer/VBoxContainer/NewGame.pressed.connect(_on_new_game_pressed)
 	$HBoxContainer/VBoxContainer/Quit.pressed.connect(_on_quit_pressed)
-
+		
 func _on_new_game_pressed():
 	get_tree().change_scene_to_file("res://scenes/menus/PlayerSelect.tscn")
 
